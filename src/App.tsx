0ca1e99d0042790459684ec777db838464d893e0
gmartins-dev <guilhermemm.dev@gmail.com>
2026-03-09 09:10:05 -0300

    chore: install shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import { Button } from '@/components/ui/button'
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
         <h1 className="text-3xl font-bold underline">
           Hello world!
         </h1>
+         <Button>ShadCnButton</Button>
       </div>
     </>
   )
